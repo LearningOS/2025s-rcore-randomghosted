9a814b31b98873b08cf250b20731266bd97c2356
randomghosted <3237200968@qq.com>
2025-06-06 01:07:29 +0000

    little change

--- os/src/task/manager.rs
+++ os/src/task/manager.rs
@@ -10,8 +10,7 @@ use alloc::sync::Arc;
 use lazy_static::*;
 ///A array of `TaskControlBlock` that is thread-safe
 pub struct TaskManager {
-    /// ready_queue
-    pub ready_queue: VecDeque<Arc<TaskControlBlock>>,
+    ready_queue: VecDeque<Arc<TaskControlBlock>>,
     
     /// The stopping task, leave a reference so that the kernel stack will not be recycled when switching tasks
     stop_task: Option<Arc<TaskControlBlock>>,
